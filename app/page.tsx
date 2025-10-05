bd5e1cae46eca5dcd002a7efe9db067dc83f0356
leofsousa <leofsousa98@gmail.com>
2025-10-05 14:49:52 -0300

    header adicionado

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import florestImage from "../florest.jpg";
+import { Header } from "../components/Header";
 export default function Home() {
   return (
     <main>
-      <div className="bg-[url('/florest.jpg')] bg-cover bg-center h-screen w-full">
+      <div
+        className="bg-cover bg-center h-screen w-full"
+        style={{ backgroundImage: `url(${florestImage.src})` }}
+      >
+        <Header />
       </div>
-      <div>
-      </div>
+      <div></div>
     </main>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,38 @@
 export const Header = () => {
-  return <header> </header>;
+  return (
+    <header className="h-12 w-full bg-black/40 border-white flex justify-between">
+      <div>
+        <h1 className="text-white p-3">Logo</h1>
+      </div>
+      <div>
+        <ul className="m-2">
+          <li>
+            <a href="#" className="text-white p-2 bg-gray-400/30 rounded-md">
+              Home
+            </a>
+            <a href="#" className="text-white p-4 text-gray-400">
+              Solutions
+            </a>
+            <a href="#" className="text-white p-4 text-gray-400">
+              Process
+            </a>
+            <a href="#" className="text-white p-4 text-gray-400">
+              Stories
+            </a>
+            <a href="#" className="text-white p-4 text-gray-400">
+              About us
+            </a>
+            <a href="#" className="text-white p-4 text-gray-400">
+              Resources
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button className="bg-primary mx-4 my-2 py-1 px-2 rounded-md">
+          Start Project
+        </button>
+      </div>
+    </header>
+  );
 };
